712690bd549e661f12a63a47c6b500720d5be71f
JoeMazich <jmazich@luc.edu>
2021-01-05 17:28:39 -0600

    Firebase deployment and Title Display

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const schedule = {
+  title: "CS Courses for 2018-2019",
+  author: "Joseph Mazich"
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.Item1}>
+        <Text style={styles.bannerStyle}>{schedule.title}</Text>
+      </View>
+
+      <View style={styles.Item2}>
+        <Text style={styles.bannerStyle2}>{schedule.author}</Text>
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
+  Item1: {
+    flex: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  Item2: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  bannerStyle: {
+    color: '#FF0000',
+    fontSize: 32,
+  },
+  bannerStyle2: {
+    color: '#00FF00',
+    fontSize: 16,
+    fontWeight: "bold",
+  }
 });
+
+export default App;
