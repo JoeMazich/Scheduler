39b2038ebed95e71de389b7545f40a13d5ede319
JoeMazich <jmazich@luc.edu>
2021-01-17 17:20:23 -0600

    firebase Databse fetch

---
+++ firebaseApp.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+import "firebase/database";
+
+const firebaseConfig =  {
+  apiKey: "AIzaSyCXYhluDDm4e0bXLK15opyHF7iZ19sb7z8",
+  authDomain: "scheduler-ff448.firebaseapp.com",
+  databaseURL: "https://scheduler-ff448-default-rtdb.firebaseio.com",
+  projectId: "scheduler-ff448",
+  storageBucket: "scheduler-ff448.appspot.com",
+  messagingSenderId: "287159545024",
+  appId: "1:287159545024:web:b47d214659eec46f3b9125"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
